012f9d1fbdaffabcc18c07f065caf09e1a534019
itgbarbara <itgbarbara@gmail.com>
2024-09-28 18:16:17 +0200

    Creating a 'Portfolio' component / section

---
+++ src/components/Porfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Porfolio = () => {
+  return (
+    <section id="portfolio">
+      <h2>Ceci est ma section portfolio</h2>
+    </section>
+  )
+}
+
+export default Porfolio
