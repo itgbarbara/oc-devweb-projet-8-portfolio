27485f041698f83e6a099fbc6ff0738762358ad3
itgbarbara <itgbarbara@gmail.com>
2024-09-28 20:37:31 +0200

    Updating the portfolio component to add the projects cards

--- src/components/Porfolio.jsx
+++ src/components/Porfolio.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProjectCards from './ProjectCards'
 
 const Porfolio = () => {
   return (
     <section id="portfolio">
       <h2>Ceci est ma section portfolio</h2>
+      <ProjectCards />
     </section>
   )
 }
