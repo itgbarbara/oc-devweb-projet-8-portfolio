15700ef2a94400d11456c0dad4a7aefd2675dddb
itgbarbara <itgbarbara@gmail.com>
2024-09-28 18:21:39 +0200

    Creating a 'Layout' component, wrapped around children elements

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <div className="body-container">
+      <Header />
+      <main className="body-container__main">{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
