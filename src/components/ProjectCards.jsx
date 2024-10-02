ce6d424a1dd75cfb03a9d065460c67bcb7d88d3d
itgbarbara <itgbarbara@gmail.com>
2024-10-02 15:50:08 +0200

    Fetching data from the public database (API call simulation) + adding content

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import projects from '../data/projects.json'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const ProjectCards = () => {
+  // Déclaration d'une variable d'état
+  const [projects, setProjects] = useState([])
+
+  // Simulation appel API
+  useEffect(() => {
+    fetch('/data/projects.json').then((response) =>
+      response
+        .json()
+        .then((projects) => {
+          setProjects(projects)
+        })
+        .catch((err) => {
+          console.log('Error: ', err)
+        })
+    )
+  }, [])
+
   return (
-    <div>
+    <div className="cards-grid">
       {projects.map((project) => (
-        <article>
-          <img src={project.cover} alt={project.title} />
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-        </article>
+        <Link
+          className="card-link"
+          to={`/project/${project.id}`}
+          key={project.id}
+        >
+          <article className="card">
+            <img
+              className="card__img"
+              src={project.cover}
+              alt={project.title}
+            />
+            <h2 className="card__title">{project.title}</h2>
+            <p className="card__description">{project.description}</p>
+          </article>
+        </Link>
       ))}
     </div>
   )
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, Navigate } from 'react-router-dom'
+import Layout from '../components/Layout'
 
 const Project = () => {
+  // Déclaration d'une variable d'état
+  const [projects, setProjects] = useState([])
+
+  // Simulation appel API
+  useEffect(() => {
+    fetch('/data/projects.json').then((response) =>
+      response
+        .json()
+        .then((projects) => {
+          setProjects(projects)
+        })
+        .catch((err) => {
+          console.log('Error: ', err)
+        })
+    )
+  }, [])
+
+  // Récupération de l'id qui se trouve dans l'url
+  const { id } = useParams()
+
+  // Méthode pour trouver l'index de l'élément dont l'id est dans l'url de la fiche projet
+  const index = projects.findIndex((project) => project.id === id)
+
   return (
-    <div>
-      <p>Voici une page dédiée à un projet</p>
-    </div>
+    <Layout>
+      {!projects[index] ? (
+        <Navigate to="/error" replace={true} />
+      ) : (
+        <h1>{projects[index].title}</h1>
+      )}
+    </Layout>
   )
 }
 
