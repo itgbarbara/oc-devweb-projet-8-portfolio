baa5ce115972caf7dbe73692bd50f9582bb80075
itgbarbara <itgbarbara@gmail.com>
2024-10-02 15:33:15 +0200

    Footer modifications (adding content, icons and style)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
-    <footer id="contact" className="footer">
-      <p>Contact</p>
+    <footer className="footer">
+      <div className="legal">
+        <p className="legal__copyright">
+          &#169; 2024 | Barbara ALVAREZ | Tous droits réservés
+        </p>
+        <div className="legal__notices">
+          <Link to="">
+            <p>Mentions légales</p>
+          </Link>
+          <Link to="">
+            <p>Plan du site</p>
+          </Link>
+        </div>
+      </div>
+      <div className="icons">
+        <Link to="https://github.com/itgbarbara" target="_blank">
+          <FontAwesomeIcon icon={faSquareGithub} className="icons__logo" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/itgbarbara/" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} className="icons__logo" />
+        </Link>
+      </div>
     </footer>
   )
 }
