0c6efcec4f4922c9a9301002eda766b3048eaaba
itgbarbara <itgbarbara@gmail.com>
2024-10-08 15:58:41 +0200

    Adding style to the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,9 @@ const Contact = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm()
+    formState: { errors, isValid },
+  } = useForm({ mode: 'onTouched' })
+
   // register : fonction de react-hook-form qui va permettre d'enregistrer les valeurs que va prendre un champ. S'utilise avec l'attribut "name"
   // Evite de passer par l'utilisation du State et gère les attributs "value" et "onChange" de chaque élément du formulaire en une fois
   // handleSubmit : fonction utilisée pour générer une autre fonction (ici, onSubmit)
@@ -25,13 +26,12 @@ const Contact = () => {
     <section id="contact" className="homepage-section">
       <h2>Contact</h2>
       <p>
-        Si vous souhaitez que l'on travaille ensemble, n'hésitez pas à me
-        contacter !
+        Si vous souhaitez que l'on travaille ensemble, n'hésitez pas à m'envoyer
+        un message !
       </p>
-      <form className="contactForm" onSubmit={handleSubmit(onSubmit)}>
-        <div className="contactForm__id">
-          <div>
-            <label htmlFor="username">Nom</label>
+      <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="contact-form__id">
+          <div className="contact-form__field contact-form__field--username">
             <input
               type="text"
               name="username"
@@ -51,8 +51,7 @@ const Contact = () => {
             />
             {errors.username && <span>{errors.username.message}</span>}
           </div>
-          <div>
-            <label htmlFor="email">Email</label>
+          <div className="contact-form__field contact-form__field--email">
             <input
               type="email"
               name="email"
@@ -68,17 +67,19 @@ const Contact = () => {
           </div>
         </div>
         <textarea
+          className="contact-form__message"
           name="message"
           id="message"
           {...register('message', {
             required: true,
             minLength: 5,
           })}
-          placeholder="Message"
+          rows="10"
+          placeholder="Votre message"
         ></textarea>
-        <p>{data}</p>
-        <input type="submit" value="Envoyer" />
+        <input disabled={!isValid} type="submit" value="Envoyer" />
       </form>
+      <p>{data}</p>
     </section>
   )
 }
