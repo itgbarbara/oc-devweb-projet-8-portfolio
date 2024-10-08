d6c68f5cc9021714645f94fb79e71c55a745d47d
itgbarbara <itgbarbara@gmail.com>
2024-10-08 14:16:28 +0200

    Adding a link to the homepage

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
     <Layout>
       <h1>Erreur 404</h1>
+      <Link to="/">Retour à la page d'accueil</Link>
     </Layout>
   )
 }
