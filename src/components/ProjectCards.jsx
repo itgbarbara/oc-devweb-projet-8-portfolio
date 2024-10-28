f08c4f35548f7a9dc02b9277542efddfc1a4d2c4
itgbarbara <itgbarbara@gmail.com>
2024-10-28 17:44:46 +0100

    Adding an overlay element appearing when cards are being hovered

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 
-const ProjectCards = () => {
-  // Déclaration d'une variable d'état
-  const [projects, setProjects] = useState([])
-
-  // Simulation appel API
-  useEffect(() => {
-    fetch('/data/projects.json').then((response) =>
-      response
-        .json()
-        .then((projects) => {
-          setProjects(projects)
-        })
-        .catch((err) => {
-          console.log('Error: ', err)
-        })
-    )
-  }, [])
-
+const ProjectCards = ({ projects }) => {
   return (
     <div className="cards-grid">
       {projects.map((project) => (
@@ -29,14 +11,20 @@ const ProjectCards = () => {
           key={project.id}
         >
           <article className="card">
-            <div className="card__overlay"></div>
+            <div className="card__overlay">
+              <p className="card__description">{project.description}</p>
+              <ul className="card__tags">
+                {project.tags.map((tag) => (
+                  <li key={`${tag}-${project.id}`}>{tag}</li>
+                ))}
+              </ul>
+            </div>
             <img
               className="card__img"
               src={project.cover}
               alt={project.title}
             />
             <h2 className="card__title">{project.title}</h2>
-            <p className="card__description">{project.description}</p>
           </article>
         </Link>
       ))}
