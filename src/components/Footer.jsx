8c074d910ba9d7538e8d16ae2c55519a982daf2a
itgbarbara <itgbarbara@gmail.com>
2024-11-06 22:26:11 +0100

    Removing legal notices link from the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -11,21 +10,6 @@ const Footer = () => {
         <p className="legal__copyright">
           &#169; 2024 | Barbara ALVAREZ | Tous droits réservés
         </p>
-        <div className="legal__notices">
-          <Link to="">
-            <p>Mentions légales</p>
-          </Link>
-          <Link to="">
-            <p>Plan du site</p>
-          </Link>
-          <a
-            href="https://github.com/itgbarbara/oc-devweb-projet-8-portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Code du site
-          </a>
-        </div>
       </div>
       <div className="icons">
         <a
