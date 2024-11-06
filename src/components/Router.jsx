be1628b820a06dd56cb68bd66184e389b5c60b97
itgbarbara <itgbarbara@gmail.com>
2024-11-06 16:58:55 +0100

    Adding the ContactModal component to the root of the app + wrapping the router in the context Providers

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,17 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Project from '../pages/Project'
 import ErrorPage from '../pages/ErrorPage'
+import DataProvider from '../utils/context/DataProvider'
+import ModalProvider from '../utils/context/ModalProvider'
+import ContactModal from './Contact/ContactModal'
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project/:id" element={<Project />} />
-          <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
+        <DataProvider>
+          <ModalProvider>
+            <ContactModal />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/project/:id" element={<Project />} />
+              <Route path="/error" element={<ErrorPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </ModalProvider>
+        </DataProvider>
       </BrowserRouter>
     </div>
   )
