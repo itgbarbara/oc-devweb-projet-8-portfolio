0cfb6551208174a48009de9c6c7c198203b9bfc1
itgbarbara <itgbarbara@gmail.com>
2024-11-10 20:01:11 +0100

    Updating the back to top button

--- src/components/BackToTopBtn.jsx
+++ src/components/BackToTopBtn.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
-import { HashLink } from 'react-router-hash-link'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpLong } from '@fortawesome/free-solid-svg-icons'
 
 const BackToTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
-    <HashLink to="/#top">
-      <FontAwesomeIcon icon={faArrowUpLong} className="back-to-top" />
-    </HashLink>
+    <div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className={`back-to-top ${isVisible && 'back-to-top--visible'}`}
+        >
+          <FontAwesomeIcon icon={faArrowUpLong} />
+        </button>
+      )}
+    </div>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import BackToTopBtn from '../components/BackToTopBtn'
 
 const Layout = ({ children }) => {
   return (
     <div id="top" className="body-container">
       <Header />
       <main className="body-container__main">{children}</main>
+      <BackToTopBtn />
       <Footer />
     </div>
   )
